d633afcc6a5cbc4262eccb4fcc205e7cc9c8bb2e
Jack Sim <jack@99.co>
2021-09-13 21:36:24 +0800

    blacked code and added tests

---
+++ tests/test_bitboard.py
@@ -0,0 +1,17 @@
+from src.bitboard import bitboard_to_coords
+
+import pytest
+
+samples = [
+    # bit, (columns, rows), (result_x, result_y)
+    (0b1, (1, 1), (0,0)),
+    (0b10, (1, 1), (0, -40)),
+    (0b1, (2, 2), (40, 40)),
+    (0b10, (2, 2), (0, 40)),
+    (0b100, (2, 2), (40, 0)),
+]
+
+@pytest.mark.parametrize("bit,size,result", samples)
+def test_bitboard_to_coords(bit, size, result):
+    columns, rows = size
+    assert bitboard_to_coords(bit, rows, columns) == result
